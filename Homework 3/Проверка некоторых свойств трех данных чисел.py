798e10638e10d8c4ec617bc3c45fc5a15e593918
bunkabakung <lilia.blyumina@gmail.com>
2016-10-16 16:09:47 +0300

    'дописала одну программу и еще две переименовала'

---
+++ Homework 3/Проверка некоторых свойств трех данных чисел.py
@@ -0,0 +1,14 @@
+a=input('Введите число а')
+b=input('Введите число b')
+c=input('Введите число c')
+a=int(a)
+b=int(b)
+c=int(c)
+if a+b==c:
+    print ('a и b в сумме дают c')
+else:
+    print ('a и b в сумме НЕ дают c')
+if a/b==c:
+    print ('a разделить на b равно c')
+else:
+    print ('a разделить на b НЕ равно c')
