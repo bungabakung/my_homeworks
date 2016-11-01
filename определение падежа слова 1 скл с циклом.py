b41b0e12bdc4633f6fcaba2f4ab4784a5bd2f816
bungabakung <lilia.blyumina@gmail.com>
2016-11-01 16:23:00 +0300

    Add files via upload

---
+++ определение падежа слова 1 скл с циклом.py
@@ -0,0 +1,24 @@
+word=input('Введите русское существительное первого склонения ')
+while len(word)!=0:
+    if word.endswith('а') or word.endswith  ('я'):
+        print ('Именительный падеж, единственное число')
+    elif word.endswith ('ами') or word.endswith ('ями'):
+        print ('Творительный падеж, множественное число')
+    elif word.endswith('ы') or word.endswith  ('и'):
+        print ('Родительный падеж, единственное число или именительный или винительный падеж, множественное число')
+    elif word.endswith('е'):
+        print ('Дательный или предложный падеж, единственное число')
+    elif word.endswith('ой') or word.endswith ('ою') or  word.endswith('ёй') or word.endswith ('ёю')or word.endswith  ('ею') or word.endswith  ('ей'):
+        print ('Творительный падеж, единственное число')
+    elif word.endswith('у') or  word.endswith('ю'):
+        print ('Винительный падеж, единственное число')
+    elif word.endswith ('ам') or word.endswith  ('ям'):
+        print ('Дательный падеж, множественное число')
+    elif word.endswith ('ах') or word.endswith('ях'):
+        print ('Предложный падеж, множественное число')
+    else:
+        print ('Родительный или винительный падеж, множественное число')
+    word=input('Введите еще одно русское существительное первого склонения ')
+print ('Вы ничего не ввели')
+    
+    
