45539338239ca783c0e8dcb177db1d1f8aef3c9a
bungabakung <lilia.blyumina@gmail.com>
2017-06-23 13:22:30 +0300

    Add files via upload
    Экзамен

---
+++ exam.py
@@ -0,0 +1,13 @@
+import os
+import re
+f1 = open('file_words.txt', 'w', encoding = 'utf-8')
+for file in os.listdir('.'):
+    if file.endswith('xhtml'):
+        f = open(file, 'r', 'Windows-1251')
+        a = f.read()
+        f.close()
+        r = re.search('<title>(.*)</title>', a)
+        if r:
+            f1.write(r.group(1) + '\t' + str(len(re.findall('<w>', a))) + '\n')
+        print('1')
+f1.close()
